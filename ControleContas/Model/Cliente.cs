0391b2177706f93a30a174427e1863a9490ca8b4
capitão blah <olucas.cdev@gmail.com>
2023-03-15 20:23:55 -0300

    criação da classe cliente

---
+++ ControleContas/Model/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleContas.Model
+{
+    public class Cliente
+    {
+/*      private string _nome;
+        private string _cpf;
+        private int _anoNasc;
+*/
+        public string Nome { get; private set; }
+        public string CPF { get; private set; }
+        public int AnoNascimento { get; private set; }  
+
+    }
+}
--- ControleContas/Program.cs
+++ ControleContas/Program.cs
@@ -3,4 +3,9 @@ using ControleContas.Model;
 
 Conta conta = new Conta("1234");
 Conta conta2 = new Conta("4321", 1000.99m);
-Console.WriteLine($"Conta: {conta.Numero}; Saldo: {conta.Saldo}");
+//Console.WriteLine($"Conta: {conta.Numero}; Saldo: {conta.Saldo}");
+
+Conta novaConta = new Conta("654321", 2341.00m);
+
+Console.WriteLine($"Nova Conta: {novaConta.Numero}; Saldo: {novaConta.Saldo}");
+Console.WriteLine($"Saldo Total: R${novaConta.Saldo + conta2.Saldo} ");
